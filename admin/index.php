38344ca3680104be119fdecdfd00ef83956582ef
JossepP <jossepparedes@gmail.com>
2020-04-08 04:50:19 -0400

    primer commit

---
+++ admin/index.php
@@ -0,0 +1,326 @@
+<?php
+include "../configs/config.php";
+include "../configs/funciones.php";
+
+if(isset($logear)){
+  $username = clear($username);
+  $password = clear($password);
+
+  $q = $mysqli->query("SELECT * FROM admins WHERE username = '$username' AND password = '$password'");
+  
+  if(mysqli_num_rows($q)>0){
+    $r = mysqli_fetch_array($q);
+    $_SESSION['id'] = $r['id'];
+    redir("./");
+  }else{
+    alert("Los datos no son validos");
+    redir("./");
+  }
+
+
+}
+
+
+if(!isset($_SESSION['id'])){
+  ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Admin Panel</title>
+</head>
+
+<body style="background: #08f;color:#fff">
+  <center>
+    <form style="padding-top:10%;" method="post" action="">
+      <div class="centrar_login">
+        <label>
+          <h2><i class="fa fa-key"></i> Iniciar Sesión Como Administrador</h2>
+        </label>
+        <div class="form-group">
+          <input style="padding:10px;color:#333;width:40%" type="text" class="form-control" placeholder="Usuario"
+            name="username" />
+        </div>
+
+        <div class="form-group">
+          <input style="padding:10px;color:#333;width:40%" type="password" class="form-control" placeholder="Contraseña"
+            name="password" />
+        </div>
+        <br><br>
+
+        <div class="form-group">
+          <button name="logear" type="submit"><i class="fa fa-sign-in"></i> Ingresar</button>
+        </div>
+      </div>
+    </form>
+  </center>
+</body>
+
+</html>
+<?php
+  die();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Admin Tienda en Linea</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/iCheck/flat/blue.css">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="plugins/morris/morris.css">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="plugins/datepicker/datepicker3.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker-bs3.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="../css/estilo.css">
+
+
+  <link rel="stylesheet" href="../fontawesome/css/all.css" />
+  <script type="text/javascript" src="../fontawesome/js/all.js"></script>
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+  <div class="wrapper">
+
+    <header class="main-header">
+      <!-- Logo -->
+      <a href="index2.html" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>A</b>TL</span>
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>Admin</b>TL</span>
+      </a>
+      <!-- Header Navbar: style can be found in header.less -->
+      <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+          <span class="sr-only">Toggle navigation</span>
+        </a>
+
+
+      </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar">
+      <!-- sidebar: style can be found in sidebar.less -->
+      <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+
+
+          <div class="pull-left image">
+          <!--    <img src="avatars/avatar.jpg" style="width:35px;height:35px;" class="img-circle" alt="User Image">
+          </div>
+          <div class="pull-left info">
+      
+          <p><?=admin_name_connected()?></p>
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            -->
+          </div>
+        </div>
+        <!-- search form -->
+        
+        <form action="#" method="get" class="sidebar-form">
+          <div class="input-group">
+            <input type="text" name="q" class="form-control" placeholder="Search...">
+            <span class="input-group-btn">
+              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+              </button>
+            </span>
+          </div>
+        </form>
+        <!-- /.search form -->
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu">
+          <li class="header">MENU</li>
+
+          <li>
+            <a href="./">
+              <i class="fa fa-th"></i> <span>Principal</span>
+            </a>
+          </li>
+
+
+          <li>
+            <a href="./?p=agregar_tgrado">
+              <i class="fa fa-th"></i> <span>Trabajo de grado</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="./">
+              <i class="fa fa-th"></i> <span>Trabajo de pasantia</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="./?p=agregar_categoria">
+              <i class="fa fa-th"></i> <span>Agregar Categoria</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="./?p=autores">
+              <i class="fa fa-th"></i> <span>Autores</span>
+            </a>
+          </li>
+
+
+        </ul>
+      </section>
+      <!-- /.sidebar -->
+    </aside>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+
+      <?php
+
+    if(!isset($p)){
+    ?>
+
+      <!-- Main content -->
+      <section class="content">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+              <div class="inner">
+                <h3>150</h3>
+
+                <p>New Orders</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <!-- ./col -->
+        </div>
+        <!-- ./col -->
+    </div>
+    <!-- /.row -->
+    <!-- Main row -->
+
+    <!-- /.row -->
+  </div>
+
+  <!-- /.box -->
+
+  </section>
+  <!-- right col -->
+  </div>
+  <!-- /.row (main row) -->
+
+  <?php
+    }else{
+      ?>
+  <div style="padding:30px;">
+    <?php
+      if(file_exists("modulos/".$p.".php")){
+        include "modulos/".$p.".php";
+      }else{
+        echo "El modulo solicitado no existe";
+      }
+      ?>
+  </div>
+  <?php
+    }
+    ?>
+
+  </section>
+  <!-- /.content -->
+  </div>
+
+  <div class="control-sidebar-bg"></div>
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- jQuery 2.2.0 -->
+  <script src="plugins/jQuery/jQuery-2.2.0.min.js"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button);
+  </script>
+  <!-- Bootstrap 3.3.6 -->
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <!-- Morris.js charts -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  <script src="plugins/morris/morris.min.js"></script>
+  <!-- Sparkline -->
+  <script src="plugins/sparkline/jquery.sparkline.min.js"></script>
+  <!-- jvectormap -->
+  <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+  <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="plugins/knob/jquery.knob.js"></script>
+  <!-- daterangepicker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+  <script src="plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- datepicker -->
+  <script src="plugins/datepicker/bootstrap-datepicker.js"></script>
+  <!-- Bootstrap WYSIHTML5 -->
+  <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+  <!-- Slimscroll -->
+  <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="plugins/fastclick/fastclick.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/app.min.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="dist/js/pages/dashboard.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+</body>
+
+</html>
---
+++ admin/modulos/agregar_tgrado.php
@@ -0,0 +1,161 @@
+<?php
+check_admin();
+
+if(isset($enviar)){
+	$name = clear($name);
+	$cedula = clear($cedula);
+	//$oferta = clear($oferta);
+	$categoria=clear($categoria);
+	$imagen = "";
+	$descargable = "";
+
+	$x = $mysqli->query("SELECT * FROM tgrado WHERE cedula = '$cedula' and name='$name'");
+
+	if(mysqli_num_rows($x)>0){
+		alert("Trabajo ya registrado");
+		redir("?p=agregar_tgrado");
+		die();
+	}
+
+
+
+	if(is_uploaded_file($_FILES['imagen']['tmp_name'])){
+		$imagen = $name.rand(0,1000).".png";
+		move_uploaded_file($_FILES['imagen']['tmp_name'], "../tgrado/".$imagen);
+	}
+
+	if(is_uploaded_file($_FILES['descargable']['tmp_name'])){
+		$descargable = rand(0,1000).$_FILES['descargable']['name'];
+		move_uploaded_file($_FILES['descargable']['tmp_name'], "../descargable/".$descargable);
+	}
+
+	$mysqli->query("INSERT INTO tgrado (name, cedula, id_categoria,imagen,descargable) VALUES ('$name','$cedula','$categoria','$imagen','$descargable')");
+	alert("Producto agregado");
+	redir("?p=agregar_tgrado");
+}
+
+if(isset($eliminar)){
+	$mysqli->query("DELETE FROM tgrado WHERE id = '$eliminar'");
+	redir("?p=agregar_tgrado");
+}
+
+
+
+
+?>
+
+<h1>Agregar Producto</h1><br><br>
+
+<form method="post" action="" enctype="multipart/form-data">
+	<div class="form-group">
+		<input type="text" class="form-control" name="name" placeholder="Titulo"/>
+	</div>
+
+
+	<div class="form-group">
+		<input type="text" class="form-control" name="cedula" placeholder="Cedula (1234567)"/>
+	</div>
+
+
+	<label>Imagen del producto</label>
+
+	<div class="form-group">
+		<input type="file" class="form-control" name="imagen" title="Imagen del producto" placeholder="Imagen del producto"/>
+	</div>
+
+	<div class="form-group">
+
+		<select name="categoria" required class="form-control">
+			<option value="">Seleccione una categoria</option>
+			<?php
+				$q = $mysqli->query("SELECT * FROM categorias ORDER BY categoria ASC");
+
+				while($r=mysqli_fetch_array($q)){
+					?>
+						<option value="<?=$r['id']?>"><?=$r['categoria']?></option>
+					<?php
+				}
+			?>
+		</select>
+
+	</div>
+
+	
+
+	<div class="form-group">
+		<label>¿Tiene algun archivo de descarga?</label>
+		<input class="form-control" type="file" name="descargable"/>
+	</div>
+
+
+	<div class="form-group">
+		<button type="submit" class="btn btn-success" name="enviar"><i class="fa fa-check"></i> Agregar Producto</button>
+	</div>
+
+</form><br>
+
+<br>
+
+<table class="table table-striped">
+
+	<tr>
+		<th>Titulo</th>
+		<th>Cedula</th>
+		<th>Autor</th>
+		<th>Imagen</th>
+		<th>Carrera</th>
+		<th>Acciones</th>
+	</tr>
+
+	<?php
+		$prod = $mysqli->query("SELECT * FROM tgrado ORDER BY id DESC");
+		while($rp=mysqli_fetch_array($prod)){
+
+
+
+			
+			$cat = $mysqli->query("SELECT * FROM categorias WHERE id = '".$rp['id_categoria']."'");
+
+			if(mysqli_num_rows($cat)>0){
+				$rcat = mysqli_fetch_array($cat);
+				$categoria = $rcat['categoria'];
+			
+			}else{
+				$categoria = "--";
+			}
+
+			$nama = $mysqli->query("SELECT * FROM autores WHERE Cedula='".$rp['cedula']."'");
+			if(mysqli_num_rows($nama)>0){
+				$xnama=mysqli_fetch_array($nama);
+				$name= $xnama['FullName'];
+			}else{
+				$name = "--";
+			}
+
+			?>
+				<tr>
+					<td><?=$rp['name']?></td>
+
+				
+
+					<td><?=$rp['cedula']?></td>
+
+					<td><?=$name?></td>
+
+					<td><img src="../tgrado/<?=$rp['imagen']?>" class="imagen_carro"/></td>
+
+
+					<td><?=$categoria?></td>
+					<td>
+						
+						<a style="color:#08f" href="?p=modificar_tgrado&id=<?=$rp['id']?>"><i class="fa fa-edit"></i></a>
+						&nbsp;
+						<a style="color:#08f" href="?p=agregar_tgrado&eliminar=<?=$rp['id']?>"><i class="fa fa-times"></i></a>
+
+					</td>
+				</tr>
+			<?php
+		}
+	?>
+
+</table>
