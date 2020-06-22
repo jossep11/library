872e39eb0a02c0aa22da8ef6e5de03454a602ed2
JossepP <jossepparedes@gmail.com>
2020-06-22 11:11:10 -0400

    1.5

--- admin/index.php
+++ admin/index.php
@@ -107,12 +107,12 @@ if(!isset($_SESSION['id'])){
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+  <![endif] test-->
 </head>
 
 <body class="hold-transition skin-blue sidebar-mini">
 
-<script type="text/javascript">
+<script type="text/javascript"> 
 var IDLE_TIMEOUT = 300; //seconds
 var _idleSecondsCounter = 0;
 document.onclick = function() {
